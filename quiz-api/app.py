97ae938379cf7e0b334e999601620e1559565afb
Victor HUGER <hugervictor@gmail.com>
2022-05-13 14:20:03 +0200

    Get quiz info - Ensure empty

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -7,5 +7,9 @@ def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
